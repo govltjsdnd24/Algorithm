f931b5908505312d5fb75d7d2191fdfacb02db1d
장선웅 <gragasolaf@gmail.com>
2023-08-07 17:45:41 +0900

    12

--- AlgorithmPractice/src/august_5/BAEKJOON_16236.java
+++ AlgorithmPractice/src/august_5/BAEKJOON_16236.java
@@ -4,16 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class fish{
+class Fish{
 	private int r,c,size;
 	
-	public fish(int r,int c,int size) {
-		this.r=r; this.c=c;
+	public Fish(int r,int c,int size) {
+		this.r=r; this.c=c;this.size=size;
 	}
 	
 	public int getR() {return r;}
 	public int getC() {return c;}
+	public void setR(int r) {this.r=r;}
+	public void setC(int c) {this.c=c;}
 	public int getSize() {return size;}
+}
+
+class Shark extends Fish{
+	private int count;
+	public Shark(int r,int c, int size) {
+		super(r,c,size);
+	}
+	public void countUp() {count++;}
+	public int getCount(){return count;}
+	public boolean comparePos(Fish fish) {
+		return (fish.getC()==this.getC() && fish.getR() == this.getR());		
+	}
 }
 
 
@@ -21,8 +35,8 @@ public class BAEKJOON_16236 {
 	
 	static int[][] sea;
 	static int N;
-	static fish sharkPos;
-	static List<fish> fishPos=new ArrayList<fish>();
+	static Shark shark;
+	static List<Fish>[] fish=new ArrayList[7];
 
 	public static void main(String[] args) {
 		Scanner scan= new Scanner(System.in);
@@ -32,22 +46,34 @@ public class BAEKJOON_16236 {
 			for (int j = 0; j < N; j++) {
 				sea[i][j]=scan.nextInt();
 				if(sea[i][j]==9)
-					sharkPos=new fish(i,j,2);
+					shark=new Shark(i,j,2);
 				else if(sea[i][j]>0)
-					fishPos.add(new fish(i,j,sea[i][j]));
+					fish[sea[i][j]].add(new Fish(i,j,sea[i][j]));
 			}
 		
 		fishRecursion(0);
-
+		scan.close();
 	}
 	
 	private static void fishRecursion(int timer) {
-		if(fishPos.isEmpty()|| fishPos.) {
+		if(fish[shark.getSize()].isEmpty()) {
 			System.out.println(timer);
 			return;
 		}
 		
+		if(shark.comparePos(fish[shark]))
 		
+
+		if(fish[shark.getSize()].get(0).getC()>shark.getC())
+			shark.setC(shark.getC()+1);
+		else if(fish[shark.getSize()].get(0).getC()<shark.getC())
+			shark.setC(shark.getC()-1);
+		if(fish[shark.getSize()].get(0).getR()>shark.getR())
+			shark.setC(shark.getR()+1);
+		else if(fish[shark.getSize()].get(0).getR()<shark.getR())
+			shark.setC(shark.getR()-1);
+		
+		fishRecursion(timer+1);
 	}
 
 }
