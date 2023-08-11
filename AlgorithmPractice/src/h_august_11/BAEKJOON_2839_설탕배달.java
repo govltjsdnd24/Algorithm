f8ee51357187ffca55cc9252bac6aba6a09bf605
장선웅 <gragasolaf@gmail.com>
2023-08-11 18:01:03 +0900

    1

--- AlgorithmPractice/src/h_august_11/BAEKJOON_2839_설탕배달.java
+++ AlgorithmPractice/src/h_august_11/BAEKJOON_2839_설탕배달.java
@@ -17,53 +17,6 @@ public class BAEKJOON_2839_설탕배달 {
 		System.out.println(recursion(N,0));
 	}
 	
-	public static int sugGreed() {
-		if(N<=2)
-			return -1;
-		int temp=N;
-		int count=0;
-		while(true) {
-			if(temp>=5) {
-				temp-=5;
-				count++;
-			}
-		}
-		
-	}
-	
-	
-	
-	public static int sugarGreedy() {
-		int bags=0;
-		int temp=N;
-		
-		if ((temp%5)%3<3 || temp%5==0 ) {
-			bags+=(temp%5)%3;
-			temp/=5;
-			bags+=temp;
-			temp/=3;
-			bags+=temp;
-		}
-		else if((temp%3)%5<3 || temp%3==0 ) {
-			bags+=(temp%3)%5;
-			temp/=3;
-			bags+=temp;
-			temp/=5;
-			bags+=temp;
-		}
-		else
-			bags=-1;
-		
-		return bags;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
 	private static int recursion(int carriable,int count) {
 		if(carriable<0)
 			return -1;
