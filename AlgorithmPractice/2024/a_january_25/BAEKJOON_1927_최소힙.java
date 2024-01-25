c120306970f765fda3979e62e8fc6f14e4be24c9
Sun Woong Chang <38126462+govltjsdnd24@users.noreply.github.com>
2024-01-25 11:59:01 +0900

    2024-01-25-min-heap-algo

---
+++ AlgorithmPractice/2024/a_january_25/BAEKJOON_1927_최소힙.java
@@ -0,0 +1,32 @@
+package a_january_25;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class BAEKJOON_1927_최소힙 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N=Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> maxHeap=new PriorityQueue<Integer>();
+		int tc;
+		int in;
+		for(tc=1;tc<=N;tc++) {
+			in=Integer.parseInt(br.readLine());
+			if(in!=0)
+				maxHeap.add(in);
+			else {
+				if(maxHeap.isEmpty())
+					System.out.println(0);
+				else
+					System.out.println(maxHeap.poll());
+			}	
+		}
+
+	}
+
+}
